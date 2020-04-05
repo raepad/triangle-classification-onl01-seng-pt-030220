3cebf78bdcfe9921df1f2757b3169b36267da464
Rachel Padworski <rachelpadworski@gmail.com>
2020-04-05 13:26:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,6 +18,22 @@ class Triangle
       
   end
   
+  def illegal_triangle
+    if one <= 0 || two <= 0 || three <= 0
+      begin
+        raise TriangleError
+      end
+    elsif one == nil || two == nil || three == nil
+      begin
+        raise TriangleError
+      end
+    elsif one + two <= three || one + three <= two || three + two <= one
+      begin
+        raise TriangleError
+      end
+    end
+  end
+  
   class TriangleError < StandardError
     
   end
